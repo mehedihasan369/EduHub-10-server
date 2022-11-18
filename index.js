c449a6c7993684dc70f51cb88a6d124e85b5f5a5
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-18 06:29:54 +0600

    secondApi

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express =require('express');
+const express = require('express');
 const app = express();
-const cors =require('cors')
+
+const cors =require('cors');
+app.use(cors());
 const port =process.env.PORT || 5000;
 
-app.use(cors());
 
 
-const categories = require('./data/categories.json')
+
+const categories = require('./data/categories.json');
+const course = require('./data/product.json');
 
 app.get('/',(req,res) => {
 res.send('Course ApI Running');
@@ -14,6 +17,23 @@ res.send('Course ApI Running');
 app.get('/categories',(req,res) => {
 res.send(categories);
 }); 
+
+const courseCollection = require("./Data/product.json");
+
+app.get("/product/:id", (req, res) => {
+    const id = req.params.id;
+    const getSingleItem = courseCollection?.find((p) => p.id == id);
+    if (!getSingleItem) {
+      res.send(" pai nai");
+    }
+    res.send(getSingleItem);
+  });
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
